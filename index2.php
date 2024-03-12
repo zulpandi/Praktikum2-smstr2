62c3c1fc14f833f6e3cfdf95fa45c1011721154a
zulpandi <146904255+zulpandi@users.noreply.github.com>
2024-03-12 16:25:15 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,35 @@
+<?php   
+
+
+$nama = 'zulfan'; 
+$umur = 19;
+$hobi = 'futsal';
+
+// echo 'WELCOME '     .    $nama   .   ' dengan umur '  .  $umur ;
+
+
+define('PHI', 3.14);
+$jari = 10;
+
+$keliling = 2 * PHI * $jari;
+$luas = PHI * $jari * $jari;
+
+// echo $keliling;
+
+// echo '<br>' . $luas;
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>   <?=   $title     ?>     </h1>
+
+</body>
+</html>
